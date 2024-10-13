747d8abd291a58b09da58b299b83c9084cd18447
Nuri Engin <dev.nuriengin@gmail.com>
2024-10-13 19:38:02 +0200

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,14 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5001;
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS for specific origins
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://testa-asta-kurdi-cda9ea8e953a.herokuapp.com/'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow all standard methods
-    credentials: true, // If you want to allow credentials like cookies or authentication tokens
+    origin: [
+      'http://localhost:3000',  // For local development
+      'https://testa-asta-kurdi-cda9ea8e953a.herokuapp.com',  // Your frontend URL on Heroku
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
   await app.listen(PORT);
